bb2a6615ac48e490d88e9e5f29261fd5eff4996d
gaoshi <258775211@qq.com>
2025-01-15 21:22:14 +0800

    增加log定义

--- Game/GameMode.cpp
+++ Game/GameMode.cpp
@@ -1,7 +1,10 @@
 #include "GameMode.h"
 
+
 // 初始化游戏
 void GameMode::InitGame(HINSTANCE hinstance) {
+    LogI("GameMode::InitGame +");
+
     // 初始化引擎
     EngineProvider::InitEngine(hinstance);
 
@@ -13,12 +16,18 @@ void GameMode::InitGame(HINSTANCE hinstance) {
 
     // 增加初始场景
     SceneManager::AddScene(new Scene_Test());
-    
+
+
+    LogI("GameMode::InitGame -");
 }
 
 // 游戏运行
 void GameMode::RunGame() {
+    LogI("GameMode::RunGame +");
+
     // 运行引擎
     EngineProvider::RunEngine();
 
+
+    LogI("GameMode::RunGame -");
 }
